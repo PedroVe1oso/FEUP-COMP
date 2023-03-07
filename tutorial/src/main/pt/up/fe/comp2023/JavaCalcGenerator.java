690a252f4435a367dfb01e82b064b62eb0977c3f
Pedro Veloso <up201906293@up.pt>
2023-03-07 10:25:44 +0000

    Point 7 - Initial content of the visitor class, called JavaCalcGenerator.

---
+++ tutorial/src/main/pt/up/fe/comp2023/JavaCalcGenerator.java
@@ -0,0 +1,7 @@
+package pt.up.fe.comp2023;
+
+import pt.up.fe.comp.jmm.ast.AJmmVisitor;
+import pt.up.fe.comp.jmm.ast.JmmNode;
+
+public class JavaCalcGenerator extends AJmmVisitor<String, String> {
+}
