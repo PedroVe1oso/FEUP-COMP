6696902f09543a1fcf42ff5fc841d7e83c45abb5
Pedro Veloso <up201906293@up.pt>
2023-03-07 19:57:00 +0000

    Mapped each kind of node to a specific method.

--- tutorial/src/main/pt/up/fe/comp2023/JavaCalcGenerator.java
+++ tutorial/src/main/pt/up/fe/comp2023/JavaCalcGenerator.java
@@ -4,4 +4,16 @@ import pt.up.fe.comp.jmm.ast.AJmmVisitor;
 import pt.up.fe.comp.jmm.ast.JmmNode;
 
 public class JavaCalcGenerator extends AJmmVisitor<String, String> {
+    private String className;
+
+    public JavaCalcGenerator(String className) {
+        this.className = className;
+    }
+
+    protected void builVisitor() {
+        addVisit("Program", this::dealWithProgram);
+        addVisit("Assignment", this::dealWithAssignment);
+        addVisit("Integer", this::dealWithLiteral);
+        addVisit("Identifier", this::dealWithLiteral);
+    }
 }
